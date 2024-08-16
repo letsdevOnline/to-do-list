b41896b5271fba77ba6439ead1f14c88a9100ab1
Let's Dev <88349338+letsdevOnline@users.noreply.github.com>
2024-08-16 06:24:57 -0400

    reCode everything for a clean Code
    used grid flex

--- script.js
+++ script.js
@@ -1,77 +1,69 @@
-const form = document.querySelector("#item-form");
-const input = document.querySelector("#item-input");
-const uList = document.querySelector("#item-list");
 const clear = document.querySelector("#clear");
-const close = document.querySelectorAll(".remove-item");
+const uList = document.querySelector("#items");
 const filter = document.querySelector("#filter");
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
 
-function onSubmit(e) {
-   e.preventDefault();
-
-   // if input empty retry
-
-   if (input.value === "") {
-      alert("enter something");
-      return;
-   }
-   //create Li
-   const li = document.createElement("li");
-   li.appendChild(document.createTextNode(input.value));
-   //create button
-   const button = createButton("remove-item btn-link text-red");
-   //create icon
-   const icon = createIcon("fa-solid fa-xmark");
-   //append child
-   button.appendChild(icon);
-   //append button to li
-   li.appendChild(button);
-   //add everything to page
-
-   uList.appendChild(li);
-   check(); // after we add li , we check that it !== 0 then run the f
-   input.value = "";
-}
-
-function createButton(classes) {
-   const button = document.createElement("button");
-   button.className = classes;
-   return button;
-}
-function createIcon(classes) {
-   const icon = document.createElement("i");
-   icon.className = classes;
-   return icon;
-}
-
-form.addEventListener("submit", onSubmit);
-
-// ----------------------------clear all--------------
-function onClick() {
+//clear button Activate
+function onClear() {
    uList.innerHTML = ``;
    check();
 }
-clear.addEventListener("click", onClick);
+clear.addEventListener("click", onClear);
 
-// ----------------- remove each item----------------
+//remove each item by click the x icon
 
 function removeItem(e) {
    if (e.target.parentElement.classList.contains("remove-item")) {
       e.target.parentElement.parentElement.remove();
-      check();
    }
+   check();
 }
 uList.addEventListener("click", removeItem);
 
-//----------------when clear all , delete filter -----------
-
+//--------check if there is no tasks delete clear n filter----
 function check() {
-   const listLi = uList.querySelectorAll("li");
-   if (listLi.length == 0) {
-      filter.style.display = "none";
+   const li = uList.querySelectorAll("li");
+   if (li.length == 0) {
       clear.style.display = "none";
+      filter.style.display = "none";
    } else {
-      filter.style.display = "block";
       clear.style.display = "block";
+      filter.style.display = "block";
    }
 }
 check();
+
+// ---------------add item when submit-------------------
+
+function onSubmit(e) {
+   e.preventDefault();
+   if (input.value == ``) {
+      alert("Enter a Task first");
+      return;
+   }
+   const li = document.createElement("li");
+   li.appendChild(document.createTextNode(input.value));
+   // add button
+   const button = createButton("remove-item btn-link text-red");
+   // append icon to button
+   const icon = createIcon("fa-solid fa-xmark");
+   button.appendChild(icon);
+   // append to to li
+   li.appendChild(button);
+   // append li to ul
+   uList.appendChild(li);
+   input.value = ""; // clear input
+   check();
+}
+function createButton(classes) {
+   const button = document.createElement("button");
+   button.classList = classes;
+   return button;
+}
+function createIcon(classes) {
+   const icon = document.createElement("i");
+   icon.classList = classes;
+   return icon;
+}
+form.addEventListener("submit", onSubmit);
